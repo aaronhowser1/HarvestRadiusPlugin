03f9fa667523b9555d552192a21c3f321d89e48b
aaron <aaronhowser1@gmail.com>
2026-01-14 12:54:38 -0600

    feat: enhance player interaction handling in HarvestRadiusPlugin

--- src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadiusPlugin.java
+++ src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadiusPlugin.java
@@ -1,5 +1,7 @@
 package dev.aaronhowser.mods.hytale.harvestradius;
 
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.event.events.player.PlayerInteractEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
@@ -9,22 +11,23 @@ public class HarvestRadiusPlugin extends JavaPlugin {
 
 	public HarvestRadiusPlugin(@Nonnull JavaPluginInit init) {
 		super(init);
+	}
 
+	@Override
+	protected void setup() {
 		for (int i = 0; i < 100; i++) {
 			getLogger().atInfo().log("Harvest Radius Plugin Initialized! Instance number: " + i);
 		}
 
-//		getEventRegistry().registerGlobal(
-//				PlayerInteractEvent.class,
-//				(event) -> {
-//					var player = event.getPlayer();
-//					var message = Message.raw("You interacted with something at position: " + event.getTargetBlock().toString());
-//					player.sendMessage(message);
-//				}
-//		);
+		getEventRegistry().registerGlobal(
+				PlayerInteractEvent.class,
+				(event) -> {
+					var player = event.getPlayer();
+					var message = Message.raw("You interacted with something at position: " + event.getTargetBlock().toString());
+					player.sendMessage(message);
+				}
+		);
 
 		getCommandRegistry().registerCommand(new TestCommand("test", "Test command!"));
-
 	}
-
 }
