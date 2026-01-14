80e35f72c35791eca297d69524a6629647133def
aaron <aaronhowser1@gmail.com>
2026-01-14 12:50:14 -0600

    refactor: rename HarvestRadius class to HarvestRadiusPlugin and update manifest

---
+++ src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadiusPlugin.java
@@ -0,0 +1,30 @@
+package dev.aaronhowser.mods.hytale.harvestradius;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class HarvestRadiusPlugin extends JavaPlugin {
+
+	public HarvestRadiusPlugin(@Nonnull JavaPluginInit init) {
+		super(init);
+
+		for (int i = 0; i < 100; i++) {
+			getLogger().atInfo().log("Harvest Radius Plugin Initialized! Instance number: " + i);
+		}
+
+//		getEventRegistry().registerGlobal(
+//				PlayerInteractEvent.class,
+//				(event) -> {
+//					var player = event.getPlayer();
+//					var message = Message.raw("You interacted with something at position: " + event.getTargetBlock().toString());
+//					player.sendMessage(message);
+//				}
+//		);
+
+		getCommandRegistry().registerCommand(new TestCommand("test", "Test command!"));
+
+	}
+
+}
