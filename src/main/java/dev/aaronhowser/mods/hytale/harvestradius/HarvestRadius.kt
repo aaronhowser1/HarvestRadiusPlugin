b3d680c193d2ea13afb464e7406ebd3082443daf
aaron <aaronhowser1@gmail.com>
2026-01-14 12:20:06 -0600

    feat: add HarvestRadius plugin implementation

---
+++ src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadius.kt
@@ -0,0 +1,12 @@
+package dev.aaronhowser.mods.hytale.harvestradius
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+
+class HarvestRadius(init: JavaPluginInit) : JavaPlugin(init) {
+
+	override fun setup() {
+		super.setup()
+	}
+
+}
