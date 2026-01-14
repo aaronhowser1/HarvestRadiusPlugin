89ef1f307e55e0e946db650b483e90f2816f6df7
aaron <aaronhowser1@gmail.com>
2026-01-14 12:25:34 -0600

    feat: add player interaction event handling to HarvestRadius plugin

--- src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadius.kt
+++ src/main/java/dev/aaronhowser/mods/hytale/harvestradius/HarvestRadius.kt
@@ -1,12 +1,21 @@
 package dev.aaronhowser.mods.hytale.harvestradius
 
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.event.events.player.PlayerInteractEvent
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
 
 class HarvestRadius(init: JavaPluginInit) : JavaPlugin(init) {
 
 	override fun setup() {
-		super.setup()
+
+		eventRegistry.registerGlobal(PlayerInteractEvent::class.java) { event ->
+			val player = event.player
+			val coordinates = event.targetBlock
+
+			player.sendMessage(Message.raw("You interacted with block at: ${coordinates.x}, ${coordinates.y}, ${coordinates.z}"))
+		}
+
 	}
 
 }
